01f74a13f6c0296a29510f997e40f126e2141d58
trappist <trappist@prod25.personifi.com>
2010-07-26 14:11:17 -0500

    Add support for per-project notifications

--- lib/notifier_hook.rb
+++ lib/notifier_hook.rb
@@ -4,53 +4,55 @@ class NotifierHook < Redmine::Hook::Listener
   @@subdomain = nil
   @@token     = nil
   @@room      = nil
+  @@projects  = []
 
   def self.load_options
-    options = YAML::load( File.open(File.join(Rails.root, 'config', 'campfire.yml')) )
+    options     = YAML::load( File.open(File.join(File.dirname(__FILE__), '..', 'config', 'campfire.yml')) )
     @@subdomain = options[Rails.env]['subdomain']
-    @@token = options[Rails.env]['token']
-    @@room = options[Rails.env]['room']
+    @@token     = options[Rails.env]['token']
+    @@room      = options[Rails.env]['room']
+    @@projects  = options[Rails.env]['projects'].split(/\s*,\s*/)
+    RAILS_DEFAULT_LOGGER.warn(options.inspect)
   end
 
   def controller_issues_new_after_save(context = { })
-    @project = context[:project]
     @issue = context[:issue]
     @user = @issue.author
-    speak "#{@user.firstname} created issue “#{@issue.subject}”. Comment: “#{truncate_words(@issue.description)}” http://#{Setting.host_name}/issues/#{@issue.id}"
+    speak "#{@user.firstname} created issue “#{@issue.subject}”. Comment: “#{truncate_words(@issue.description)}” http://#{Setting.host_name}/issues/#{@issue.id}", @issue.project
   end
   
   def controller_issues_edit_after_save(context = { })
-    @project = context[:project]
     @issue = context[:issue]
     @journal = context[:journal]
     @user = @journal.user
-    speak "#{@user.firstname} edited issue “#{@issue.subject}”. Comment: “#{truncate_words(@journal.notes)}”. http://#{Setting.host_name}/issues/#{@issue.id}"
+    speak "#{@user.firstname} edited issue “#{@issue.subject}”. Comment: “#{truncate_words(@journal.notes)}”. http://#{Setting.host_name}/issues/#{@issue.id}", @issue.project
   end
 
   def controller_messages_new_after_save(context = { })
-    @project = context[:project]
     @message = context[:message]
     @user = @message.author
-    speak "#{@user.firstname} wrote a new message “#{@message.subject}” on #{@project.name}: “#{truncate_words(@message.content)}”. http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak "#{@user.firstname} wrote a new message “#{@message.subject}” on #{@project.name}: “#{truncate_words(@message.content)}”. http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}", @message.board.project
   end
   
   def controller_messages_reply_after_save(context = { })
-    @project = context[:project]
     @message = context[:message]
     @user = @message.author
-    speak "#{@user.firstname} replied a message “#{@message.subject}” on #{@project.name}: “#{truncate_words(@message.content)}”. http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}"
+    speak "#{@user.firstname} replied a message “#{@message.subject}” on #{@project.name}: “#{truncate_words(@message.content)}”. http://#{Setting.host_name}/boards/#{@message.board.id}/topics/#{@message.root.id}#message-#{@message.id}", @message.board.project
   end
   
   def controller_wiki_edit_after_save(context = { })
     @project = context[:project]
     @page = context[:page]
     @user = @page.content.author
-    speak "#{@user.firstname} edited the wiki “#{@page.pretty_title}” on #{@project.name}. http://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}"
+    speak "#{@user.firstname} edited the wiki “#{@page.pretty_title}” on #{@project.name}. http://#{Setting.host_name}/projects/#{@project.identifier}/wiki/#{@page.title}", @project
   end
 
 private
-  def speak(message)
+  def speak(message,project=nil)
     NotifierHook.load_options unless @@subdomain && @@token && @@room
+    RAILS_DEFAULT_LOGGER.error "Projects: #{@@projects.inspect}"
+    RAILS_DEFAULT_LOGGER.error "#{project}: #{message}"
+    return true unless @@projects.include? project.identifier
     begin
       campfire = Tinder::Campfire.new @@subdomain, :token => @@token
       room = campfire.find_room_by_name(@@room)
